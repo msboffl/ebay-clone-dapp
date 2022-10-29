a4b120468775c0831832fe1dcde179f9d2eb3ac3
Mareedu Saibabu <marsaiba@WKMZT22907E9.global.publicisgroupe.net>
2022-10-29 16:52:45 +0530

    Listing Items Adde

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useMetamask, useAddress, useDisconnect } from "@thirdweb-dev/react";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/EBay_logo.png";
+
 import {
   ChevronDownIcon,
   BellIcon,
@@ -45,8 +48,50 @@ const Header = (props: Props) => {
       </nav>
       <hr className="mt-2 p-2" />
 
-      <section>
-        <div></div>
+      <section className="flex items-center space-x-2 py-5">
+        <div className="h-16 w-16 sm:w-28 md:w-35 cursor-pointer">
+          <Link href="/">
+            <Image
+              className="w-full h-full object-contain"
+              src={logo}
+              alt="Ebay Logo"
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <button className="hidden lg:flex items-center space-x-2 w-20">
+          <p className="text-sm text-gray-600">Shop by Category</p>
+          <ChevronDownIcon className="h-4 flex-shrink-0" />
+        </button>
+        <div className="flex items-center space-x-2 px-2 md:px-5 py-2 border-black border-2 flex-1">
+          <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+          <input
+            className="flex-1 outline-none"
+            type="text"
+            placeholder="Search for anything"
+          />
+        </div>
+        <button className="hidden sm:inline bg-blue-600 text-white px-5 py-2 border-blue-600 border-2">Search</button>
+        <Link href="/create">
+          <button className="border-blue-600 border-2 px-5 py-2 hover:bg-blue-300 hover:text-white">List Items</button>
+        </Link>
+      </section>
+      <hr />
+      <section className="flex space-x-6 py-4 text-xs md:text-sm justify-center whitespace-nowrap px-6">
+        <p className="link">Home</p>
+        <p className="link">Electronics</p>
+        <p className="link">Computers</p>
+        <p className="link hidden sm:inline">Video Games</p>
+        <p className="link hidden sm:inline">Home & Garden</p>
+        <p className="link hidden md:inline">Health & Beaty</p>
+        <p className="link hidden md:inline">Collectibles & Art</p>
+        <p className="link hidden lg:inline">Books</p>
+        <p className="link hidden lg:inline">Music</p>
+        <p className="link hidden xl:inline">Deals</p>
+        <p className="link hidden xl:inline">Other</p>
+        <p className="link">More</p>
+
       </section>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,82 @@
-import Header from "../components/Header"
-
+import Header from "../components/Header";
+import {
+  useContract,
+  useActiveListings,
+  MediaRenderer,
+} from "@thirdweb-dev/react";
+import { ListingType } from "@thirdweb-dev/sdk";
+import { BanknotesIcon, ClockIcon } from "@heroicons/react/24/outline";
 const Home = () => {
+  const { contract } = useContract(
+    process.env.NEXT_PUBLIC_NFT_MARKET_LISTING,
+    "marketplace"
+  );
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(contract);
+
+  console.log(listings);
   return (
     <div className=".">
-      <Header/>
-    </div>
-  )
-}
+      <Header />
 
-export default Home
+      <main className="max-w-6xl mx-auto p-5">
+        {loadingListings ? (
+          <p className="animate-pulse text-center text-blue-500">
+            Loading Listings...
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
+            {listings?.slice(2)?.map((listing) => (
+              <div
+                className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out"
+                key={listing.id}
+              >
+                <div className="flex-1 flex flex-col pb-2 items-center">
+                  <MediaRenderer className="h-32" src={listing.asset.image} />
+                </div>
+                <div className="pt-2 space-y-3">
+                  <div>
+                    <h2 className="text-lg truncate">{listing.asset.name}</h2>
+                    <hr />
+                    <p className="text-sm truncate text-gray-600">
+                      {listing.asset.description}
+                    </p>
+                  </div>
+                  <p className="">
+                    <span className="font-bold mr-2">
+                      {listing.buyoutCurrencyValuePerToken.displayValue}
+                    </span>
+                    {listing.buyoutCurrencyValuePerToken.symbol}
+                  </p>
+
+                  <div
+                    className={`flex items-center space-x-1 justify-end text-xs w-fit ml-auto p-2 rounded-lg
+                     text-white ${
+                       listing.type === ListingType.Direct
+                         ? "bg-blue-500"
+                         : "bg-red-500"
+                     }
+                  `}
+                  >
+                    <p>
+                      {listing.type === ListingType.Direct
+                        ? "Buy Now"
+                        : "Auction"}
+                    </p>
+                    {listing.type === ListingType.Direct ? (
+                      <BanknotesIcon className="h-4" />
+                    ) : (
+                      <ClockIcon className="h-4" />
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
